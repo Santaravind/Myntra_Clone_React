9d0354dbb62b9bda2df8a5d6f59e4e4c2028b44b
Aravind Sant <aravindsant563@gmail.com>
2024-11-04 09:51:11 +0530

    Frontent setup complit

---
+++ Myntra_react/src/components/HomeItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function HomeItem({item}) {
+  return (
+    <div>
+       <div class="item-container">
+      <img class="item-image" src={item.image} alt="item image" />
+      <div class="rating">
+          {item.rating.stars} ⭐ | {item.rating.count}
+      </div>
+      <div class="company-name">{item.company}</div>
+      <div class="item-name">{item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs {item.current_price}</span>
+          <span class="original-price">Rs {item.original_price}</span>
+          <span class="discount">({item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick={()=>console.log("item was clicked")}>Add to Bag</button>
+    </div>
+    </div>
+  )
+}
+
+export default HomeItem
---
+++ Myntra_react/src/routes/Bag.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import BagSummery from '../components/BagSummery'
+import BagItem from '../components/BagItem'
+
+function Bag() {
+  const item={
+    id: '001',
+    image: 'images/1.jpg',
+    company: 'Carlton London',
+    item_name: 'Rhodium-Plated CZ Floral Studs',
+    original_price: 1045,
+    current_price: 606,
+    discount_percentage: 42,
+    return_period: 14,
+    delivery_date: '10 Oct 2023',
+    rating: {
+        stars: 4.5,
+        count: 1400,
+    },
+}
+
+  return (
+    <>
+      
+    <main>
+      <div classNameName="bag-page">
+        <div classNameName="bag-items-container">
+         <BagItem item={item}/>
+        </div>
+        <BagSummery/>
+
+      </div>
+    </main>
+    
+    </>
+  )
+}
+
+export default Bag
